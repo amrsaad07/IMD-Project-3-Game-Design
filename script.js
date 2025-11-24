f550f02b818b5f193c1ded3494c95344ddeea110
Amr Saad <amrsaad@cmail.carleton.ca>
2025-11-24 11:22:36 -0500

    Trying to make Character move using keyboard

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+
+
+//VARIABLES
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
- //rectangle
-
-
- ctx.stroke();
 
 let currentX = 20;
 let currentY = 40;
@@ -14,19 +13,35 @@ const squareHeight = 50;
 var x = 0;
 var y = 0;
 
+//CANVAS SQUARES LOOP
+
 for (let y = 0; y + squareHeight <= canvas.height;y+= squareHeight){
     for (let x =0; x + squareWidth <= canvas.width; x+= squareWidth){
         ctx.rect(x,y,squareWidth,squareHeight);
     }
 }
 
+//CHARACTER
+
 var img = new Image()
 
 img.src = "Images/pixel_character.svg";
 
-img.addEventListener("load",(e) => {
-    ctx.drawImage (img, 0, 0, 50, 50)
-})
+window.addEventListener('keydown', KeyDown);
+
+
+function Draw()
+{
+	ctx.drawImage(img,x,y);
+}
+
+function KeyDown()
+{
+	if(event.key == "ArrowRight")
+	{
+		x++;
+	}
+}
 
 
 ctx.stroke();
